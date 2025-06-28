896625623f09b9a8776ed78ffb3e86824f6cac45
Zelalem <zelalemg935@gmail.com>
2025-06-28 13:37:19 +0000

    Sync LeetCode submissions - Concatenation of Array - Runtime - 1 ms (96.05%), Memory - 45.5 MB (99.71%)

---
+++ leetcode/2058-concatenation-of-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        int j=0;
+
+        for(int i=0;i<ans.length;i++){
+            if(j == nums.length){
+                j=0;
+            }
+            ans[i] = nums[j];
+            j++;
+        }
+        return ans;
+    }
+}
