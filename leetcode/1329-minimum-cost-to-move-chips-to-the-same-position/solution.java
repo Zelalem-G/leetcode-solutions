1530f350bd43c7fa711a443493620a7e1639feb2
Zelalem <zelalemg935@gmail.com>
2025-07-06 18:31:19 +0000

    Sync LeetCode submissions - Minimum Cost to Move Chips to The Same Position - Runtime - 0 ms (100.00%), Memory - 41.2 MB (100.00%)

---
+++ leetcode/1329-minimum-cost-to-move-chips-to-the-same-position/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd=0;
+        int even=0;
+
+        for(int n : position){
+            if(n%2==0){
+                even++;
+            } else{
+                odd++;
+            }
+        }
+
+        return Math.min(even,odd);
+    }
+}
