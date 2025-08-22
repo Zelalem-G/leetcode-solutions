14c51666ce567eed9aa6643db7eaf6b29ccaec85
Zelalem <zelalemg935@gmail.com>
2025-08-22 18:03:14 +0000

    Sync LeetCode submissions - Longest Repeating Character Replacement - Runtime - 8 ms (70.85%), Memory - 45.5 MB (99.48%)

---
+++ leetcode/0424-longest-repeating-character-replacement/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int l = 0, maxFreq = 0, res = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            freq[s.charAt(r) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(r) - 'A']);
+
+            while ((r - l + 1) - maxFreq > k) {
+                freq[s.charAt(l) - 'A']--;
+                l++;
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
