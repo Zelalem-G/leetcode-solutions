cdddab4b46d31752d0a0a0feafe97cdbb2277bb5
Zelalem <zelalemg935@gmail.com>
2025-08-29 18:21:40 +0000

    Sync LeetCode submissions - Count Negative Numbers in a Sorted Matrix - Runtime - 0 ms (100.00%), Memory - 45.4 MB (99.81%)

---
+++ leetcode/1476-count-negative-numbers-in-a-sorted-matrix/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count=0;
+
+        for(int[] g : grid){
+            int l=0,r=g.length-1;
+            int s=g.length;
+
+            while(l<=r){
+                int m=l+(r-l)/2;
+                if(g[m]>=0){
+                    l=m+1;
+                } else {
+                    r=m-1;
+                }
+            }
+            count+= s-l;
+        }
+
+        return count;
+    }
+}
