74b49ef2b031251027373553b58dc0f8cc985c7b
Zelalem <zelalemg935@gmail.com>
2025-09-16 22:12:38 +0000

    Sync LeetCode submissions - Max Number of K-Sum Pairs - Runtime - 18 ms (99.48%), Memory - 57.7 MB (99.98%)

---
+++ leetcode/1798-max-number-of-k-sum-pairs/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int op =0;
+        int l=0,r=nums.length-1;
+
+        while(l<r){
+            if(nums[l]+nums[r]==k){
+                op++;
+                l++;
+                r--;
+            } else if(nums[l]+nums[r]<k){
+                l++;
+            } else{
+                r--;
+            }
+        }
+
+        return op;
+    }
+}
