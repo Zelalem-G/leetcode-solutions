d3a0320c370b7d70286b2d27f88d8b0ca964f673
Zelalem <zelalemg935@gmail.com>
2025-11-30 13:55:12 +0000

    Sync LeetCode submissions - Time Needed to Buy Tickets - Runtime - 0 ms (100.00%), Memory - 42.9 MB (79.75%)

---
+++ leetcode/2195-time-needed-to-buy-tickets/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        int n = tickets.length;
+        
+        for (int i = 0; i < n; i++) {
+            if (i <= k) time += Math.min(tickets[i], tickets[k]);
+            else time += Math.min(tickets[i], tickets[k] - 1);
+        }
+        
+        return time;
+    }
+}
+
