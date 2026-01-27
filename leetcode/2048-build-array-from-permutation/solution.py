7832002e4fbe76f766ed78de003698b90aa01f5c
Zelalem <zelalemg935@gmail.com>
2026-01-27 19:33:25 +0000

    Sync LeetCode submissions - Build Array from Permutation - Runtime - 0 ms (100.00%), Memory - 12.6 MB (71.10%)

---
+++ leetcode/2048-build-array-from-permutation/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [nums[num] for num in nums]
+        return ans
