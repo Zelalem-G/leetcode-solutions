cda2a7c3bd9fc78b4cb68425a13d9f0291e1e426
Zelalem <zelalemg935@gmail.com>
2026-02-18 18:47:25 +0000

    Sync LeetCode submissions - Minimum Number of Moves to Seat Everyone - Runtime - 3 ms (57.86%), Memory - 12.4 MB (89.31%)

--- leetcode/2148-minimum-number-of-moves-to-seat-everyone/solution.py
+++ leetcode/2148-minimum-number-of-moves-to-seat-everyone/solution.py
@@ -2,8 +2,11 @@ class Solution(object):
     def minMovesToSeat(self, seats, students):
         seats.sort()
         students.sort()
-        res = 0
+
+        ans = 0
+
         for i in range(len(seats)):
-            res += abs(seats[i] - students[i])
-        return res
+            ans += abs(seats[i] - students[i])
+        
+        return ans
 
