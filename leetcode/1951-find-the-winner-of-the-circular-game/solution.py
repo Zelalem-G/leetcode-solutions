f0a688e5e7f2950fe888aa540316e392a85ecb1a
Zelalem <zelalemg935@gmail.com>
2026-02-18 11:16:27 +0000

    Sync LeetCode submissions - Find the Winner of the Circular Game - Runtime - 0 ms (100.00%), Memory - 12.3 MB (100.00%)

--- leetcode/1951-find-the-winner-of-the-circular-game/solution.py
+++ leetcode/1951-find-the-winner-of-the-circular-game/solution.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def findTheWinner(self, n, k):
-        players = list(range(1, n + 1))
-        idx = 0
-
-        while len(players) > 1:
-            idx = (idx + k - 1) % len(players)
-            players.pop(idx)
-
-        return players[0]
+        res = 0
+        for i in range(1, n+1):
+            res = (res + k) % i
+        return res + 1
 
