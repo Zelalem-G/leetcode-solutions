9f2a90195d7fa07a349ff0c888b07ecc63842c6c
Zelalem <zelalemg935@gmail.com>
2026-02-19 18:22:49 +0000

    Sync LeetCode submissions - Number of Boomerangs - Runtime - 536 ms (29.11%), Memory - 12.6 MB (68.35%)

--- leetcode/0447-number-of-boomerangs/solution.py
+++ leetcode/0447-number-of-boomerangs/solution.py
@@ -13,10 +13,9 @@ class Solution(object):
                 dy = points[i][1] - points[j][1]
                 d = dx*dx + dy*dy
 
-                freq[d] = freq.get(d, 0) + 1
-
-            for k in freq:
-                res += freq[k] * (freq[k] - 1)
+                count = freq.get(d, 0)
+                res += 2 * count
+                freq[d] = count + 1
 
         return res
 
