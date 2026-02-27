d78d7adb2eabbae5efa7a037e7e05dd449669086
Zelalem <zelalemg935@gmail.com>
2026-02-27 16:22:13 +0000

    Sync LeetCode submissions - Merge Sorted Array - Runtime - 0 ms (100.00%), Memory - 12.4 MB (90.09%)

--- leetcode/0088-merge-sorted-array/solution.py
+++ leetcode/0088-merge-sorted-array/solution.py
@@ -1,14 +1,5 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        c=0
-        for i in range(m,len(nums1)):
-            v = nums2[c]
-            c += 1
-            j= i-1
-
-            while j>=0 and nums1[j] > v:
-                nums1[j+1] = nums1[j]
-                j -= 1
-            nums1[j+1] = v
-
-        
+      for j in range(n):
+          nums1[m+j] = nums2[j]
+      nums1.sort()
