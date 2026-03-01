d098eb010012bee4f05e00287c4aefeaab210ab9
Zelalem <zelalemg935@gmail.com>
2026-03-01 16:49:23 +0000

    Sync LeetCode submissions - Maximum Number of Coins You Can Get - Runtime - 93 ms (94.96%), Memory - 19.3 MB (17.99%)

--- leetcode/1683-maximum-number-of-coins-you-can-get/solution.py
+++ leetcode/1683-maximum-number-of-coins-you-can-get/solution.py
@@ -1,15 +1,3 @@
 class Solution(object):
-    def maxCoins(self, piles):
-        piles.sort(reverse=True)
-        print(piles)
-        ans = 0
-        my= 1
-        bob = len(piles)-1
-
-        #for i in range(1,len(piles),3)
-        while my<bob:
-            ans += piles[my]
-            print(piles[my])
-            my += 2
-            bob -=1
-        return ans
+        def maxCoins(self, A):
+            return sum(sorted(A)[len(A) / 3::2])
