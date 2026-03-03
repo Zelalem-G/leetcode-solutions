b385499e84e0ef9f5897825f18c556c3adc14b11
Zelalem <zelalemg935@gmail.com>
2026-03-03 20:17:42 +0000

    Sync LeetCode submissions - Container With Most Water - Runtime - 115 ms (70.96%), Memory - 20.7 MB (91.74%)

---
+++ leetcode/0011-container-with-most-water/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxArea(self, height):
+        l,r = 0, len(height)-1
+        max_area = 0
+
+        while l<r:
+            temp_area = (r-l) * min(height[l],height[r])
+
+            max_area = max(temp_area, max_area)
+
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return max_area 
+
+        
