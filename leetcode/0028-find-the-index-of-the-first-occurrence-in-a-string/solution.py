98662b5838451f74d441761bda48babb25ba5df7
Zelalem <zelalemg935@gmail.com>
2026-03-03 20:38:26 +0000

    Sync LeetCode submissions - Find the Index of the First Occurrence in a String - Runtime - 3 ms (22.33%), Memory - 12.5 MB (53.98%)

---
+++ leetcode/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack)-len(needle) +1):
+            for j in range(len(needle)):
+                if needle[j] != haystack[i+j]:
+                    break
+                if j == len(needle) -1:
+                    return i
+        return -1
+
+
+        
